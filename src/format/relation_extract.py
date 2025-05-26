6d71c703005e374d56d69a77b736791a17e13725
mass-machine <69417459+mass-machine@users.noreply.github.com>
2025-05-26 21:23:43 +0800

    Update relation_extract.py

--- src/format/relation_extract.py
+++ src/format/relation_extract.py
@@ -2,9 +2,8 @@ import json
 import logging
 
 # from config import args
-prompt_prefix = '''Given a sentence, and all entities within the sentence. 
-Extract all relationships between entities which directly stated in the sentence.
-Every relationship stated as a triple: (E_A, E_B, Relation).\nSentence: '''
+prompt_prefix = '''Given a context and all the entities within it, please extract the relationships associated with these entities and form multiple triplet relations. 
+The format should be (Ei, Ej, r).\nContext: '''
 
 prompt_suffix = '''\nRelation: '''
 prompt_suffix1='''\n Please extract the relationship according to the above requirements, do not generate code.do not explain. you just need output the entities relations:'''
